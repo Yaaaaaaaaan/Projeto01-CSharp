4d4d43dd7533168323dd292a4231fb534aeab7f4
Yan Fonseca <yangoular@gmail.com>
2023-01-22 09:00:40 -0300

    a

--- Projeto01/Program.cs
+++ Projeto01/Program.cs
@@ -10,7 +10,6 @@ namespace Projeto01
         {
             int op, v1I, v2I, v3I, v4I, totalI, idFu;
             double pi, v1D, v2D, totalD;
-
             Console.WriteLine("");
             Console.WriteLine("--x-- Exercício 01 Estrutura sequencial --x--");
             Console.WriteLine("Olá, selecione a operação digitando o número correspondente; ");
@@ -65,15 +64,31 @@ namespace Projeto01
                     v2D = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
                     Console.WriteLine("Horas trabalhadas; ");
                     v1I= int.Parse(Console.ReadLine());
-
                     totalD = v2D * v1I;
                     Console.WriteLine("Olá, " +Nome+ ", seu código é " +idFu+ ", trabalhastes " +v1I+ " hora(s), seu valor hora é " +v2D+ ". O seu total a receber será; " +totalD.ToString("F2", CultureInfo.InvariantCulture));
-                    
                     break;
                 case 5:
                     Console.WriteLine("--x-- Soma produtos --x--");
-
-
+                    Console.WriteLine("Código do produto 1; ");
+                    v1I = int.Parse(Console.ReadLine());
+                    Console.WriteLine("quantidade; ");
+                    v2I = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Valor unitário; ");
+                    v1D = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.WriteLine("Código do produto 2; ");
+                    v3I = int.Parse(Console.ReadLine());
+                    Console.WriteLine("quantidade; ");
+                    v4I = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Valor unitário; ");
+                    v2D = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    totalI = v2I + v4I;
+                    totalD = v2I * v1D + v4I * v2D;
+                    Console.WriteLine("COD.  |  QTD.  |  Valor Unit.");
+                    Console.WriteLine(v1I + "   |   " + v2I + "   |   " + v1D);
+                    Console.WriteLine(v3I + "   |   " + v4I + "   |   " + v2D);
+                    Console.WriteLine("");
+                    Console.WriteLine("total itens " + totalI);
+                    Console.WriteLine("TOTAL " + totalD);
                     break;
                 case 6:
                     Console.WriteLine("--x-- Calculo triângulo --x--");
